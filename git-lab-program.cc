4147b9baaa6fdba778b75ba77d5bc5e5417f1042
Elijah Beard <eb723320@ohio.edu>
2022-05-13 22:30:39 -0400

    example code for git lab assignment

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Elijah Beard
+ *        Date: 5-13-2022
+ * Description: Git Lab program for intro class
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
